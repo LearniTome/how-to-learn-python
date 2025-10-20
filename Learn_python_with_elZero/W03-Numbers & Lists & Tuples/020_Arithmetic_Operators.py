98e16c2dccaa9b9e682f98a1afdddfd7f8424c8c
LearniTome <LearniTome@users.noreply.github.com>
2025-10-20 21:22:13 +0100

    37- Add 020_Arithmetic Operations

---
+++ Learn_python_with_elZero/W03-Numbers & Lists & Tuples/020_Arithmetic_Operators.py
@@ -0,0 +1,79 @@
+# --------------------------
+# -- Arithmetic Operators --
+# --------------------------
+
+# Strating Course : Arithmetic Operators in Python
+print(f"{'Starting Course - Arithmetic Operators in Python'}")
+
+
+# 01- [+] Addition
+# 02- [-] Subtraction
+# 03- [*] Multiplication
+# 04- [/] Division
+# 05- [%] Modulus
+# 06- [**] Exponent
+# 07- [//] Floor Division
+# --------------------------
+
+# 01- Addition ==) [+] = Adds Two Values Together and Returns the Result
+
+# Examples of Addition Operator [+]
+
+print(10 + 30)  # output is = 40
+print(-10 + 20)  # output is = 10
+print(1 + 2.66)  # output is = 3.66
+print(1.2 + 1.2)  # output is = 2.4
+
+# 02- Subtraction ==) [-] = Subtracts the Right Value from the Left Value and Returns the Result
+
+# Examples of Subtraction Operator [-]
+
+print(60 - 30)  # output is = 30
+print(-30 - 20)  # output is = -50
+print(-30 - -20)  # output is = -10
+print(5.66 - 3.44)  # output is = 2.22
+
+# 03- Multiplication ==) [*] = Multiplies Two Values Together and Returns the Result
+
+# Examples of Multiplication Operator [*]
+
+print(10 * 3)  # 30
+print(5 + 10 * 100)  # 1005
+print((5 + 10) * 100)  # 1500
+
+# 04- Division ==) [/] = Divides the Left Value by the Right Value and Returns the Result as Float
+
+# Examples of Division Operator [/]
+
+print(100 / 20)  # 5.0
+print(int(100 / 20)) # 5
+
+# 05- Modulus ==) [%] = Divides the Left Value by the Right Value and Returns the Remainder
+
+# Examples of Modulus Operator [%]
+
+print(8 % 2)  # 0
+print(9 % 2)  # 1
+print(20 % 5)  # 0
+print(22 % 5)  # 2
+
+# 06- Exponent ==) [**] = Performs Exponential (Power) Calculation on Operators
+
+# Examples of Exponent Operator [**]
+
+print(2 ** 5)  # 32
+print(2 * 2 * 2 * 2 * 2)  # 32
+print(5 ** 4)  # 625
+print(5 * 5 * 5 * 5)  # 625
+
+# 07- Floor Division ==) [//] = Divides the Left Value by the Right Value and Returns the Largest Integer Less Than or Equal to the Result
+
+print(100 // 20)  # 5
+print(119 // 20)  # 5
+print(120 // 20)  # 6
+print(140 // 20)  # 7
+print(142 // 20)  # 7
+
+# End of Course - Arithmetic Operators in Python
+print(f"{'End of Course - Arithmetic Operators in Python'}")
+# -------------------------------------------------------------------------------------------------------------
